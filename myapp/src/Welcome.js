85f5cd5b9a52317acbbd6cfb220ae81fc0d7a888
Giuseppe Oliveri <peppe7453@gmail.com>
2023-05-29 10:02:25 +0200

    exercise 02 react

---
+++ myapp/src/Welcome.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Welcome extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome, {this.props.name}!</p>
+      </div>
+    );
+  }
+}
