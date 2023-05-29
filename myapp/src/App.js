572733e525948416cac07697811ee6088f46d989
Giuseppe Oliveri <peppe7453@gmail.com>
2023-05-29 09:48:44 +0200

    exercise 01 react

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+import Hello from "./Hello";
+import Message from "./Message";
+
+function App() {
+  return (
+    <div>
+      <Hello />
+      <Message />
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/Message.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Message extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>It's time to learn React!!!</p>
+      </div>
+    );
+  }
+}
