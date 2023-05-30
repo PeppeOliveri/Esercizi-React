92f5ca40eb8e6d11f5dcee917dfe44e9260e4e81
Giuseppe Oliveri <peppe7453@gmail.com>
2023-05-30 23:10:32 +0200

    exercise 10 react

--- myapp/src/Welcome.js
+++ myapp/src/Welcome.js
@@ -5,13 +5,16 @@ export default class Welcome extends React.Component {
   render() {
     return (
       <div>
-        <p>
-          Welcome, {this.props.name} <Age age="44" />
-        </p>
+        {this.props.name === "John" ? (
+          <p>Welcome, {this.props.name}</p>
+        ) : (
+          <p>Invalid username</p>
+        )}
+        <Age age="33" />
       </div>
     );
   }
   static defaultProps = {
-    name: <strong>Gigi</strong>,
+    name: "John",
   };
 }
