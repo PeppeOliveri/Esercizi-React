49fd550272e66a8b8303da06d2813d445998769c
Giuseppe Oliveri <peppe7453@gmail.com>
2023-05-31 10:42:25 +0200

    exercise 13 react

--- mycount/src/Counter.js
+++ mycount/src/Counter.js
@@ -2,18 +2,25 @@ import React from "react";
 
 export default class Counter extends React.Component {
   state = {
-    count: 0,
+    count: this.props.count,
   };
   constructor(props) {
     super(props);
     setInterval(() => {
       this.setState((state) => {
         return {
-          count: state.count + 1,
+          count: state.count + this.props.increment,
         };
       });
-    }, 1000);
+    }, this.props.interval);
   }
+
+  static defaultProps = {
+    count: 5,
+    increment: 2,
+    interval: 5000,
+  };
+
   render() {
     return <h1>Count: {this.state.count} </h1>;
   }
