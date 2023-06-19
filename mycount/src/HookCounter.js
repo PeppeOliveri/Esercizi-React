67af92b7dd37c3edf96761905fa8871f9903f3b4
Giuseppe Oliveri <peppe7453@gmail.com>
2023-06-19 12:41:48 +0200

    exercise 42 react

--- mycount/src/HookCounter.js
+++ mycount/src/HookCounter.js
@@ -1,16 +1,29 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export function HookCounter({ initialValue = 0 }) {
   const [counter, setCounter] = useState(initialValue);
 
-  function handleIncrement() {
+  const handleIncrement = useCallback(function handleIncrement() {
     setCounter((c) => c + 1);
-  }
+  }, []);
+
+  const handleDecrement = useCallback(function handleDecrement() {
+    setCounter((c) => c - 1);
+  }, []);
+
+  const handleReset = useCallback(
+    function handleReset() {
+      setCounter(initialValue);
+    },
+    [initialValue]
+  );
 
   return (
     <div>
       <h2>Counter: {counter}</h2>
       <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
