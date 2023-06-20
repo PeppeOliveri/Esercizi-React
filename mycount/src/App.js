6ab2ca64d865737d17fa21802160f7e4f6512aab
Giuseppe Oliveri <peppe7453@gmail.com>
2023-06-20 16:50:47 +0200

    exercise 47 react

--- mycount/src/App.js
+++ mycount/src/App.js
@@ -4,6 +4,7 @@ import { Route } from "react-router-dom";
 import Page1 from "./Page1";
 import Page2 from "./Page2";
 import Page3 from "./Page3";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
@@ -12,6 +13,15 @@ function App() {
         <Route path="/" element={<Page1 />} />
         <Route path="/page2" element={<Page2 />} />
         <Route path="/page3" element={<Page3 />} />
+        <Route
+          path="*"
+          element={
+            <div>
+              <h1>Not found</h1>
+              <Link to="/"> Go back </Link>
+            </div>
+          }
+        />
       </Routes>
     </>
   );
