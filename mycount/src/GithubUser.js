ea2916a8a609aff37e39e6bf9b44729546fccc14
Giuseppe Oliveri <peppe7453@gmail.com>
2023-06-21 10:52:06 +0200

    exercise 52 react

--- mycount/src/GithubUser.js
+++ mycount/src/GithubUser.js
@@ -2,13 +2,14 @@ import React from "react";
 import { UseGithubUser } from "./UseGithubUser";
 
 export default function GithubUser({ username }) {
-  const { users, error } = UseGithubUser({ username });
+  const { users, error, refresh } = UseGithubUser({ username });
 
   return (
     <div>
       <p>Nome: {users && users.name}</p>
       <p>Link: {users && users.url}</p>
       {error && <h3>An error has occurred</h3>}
+      <button onClick={refresh}>Refresh</button>
     </div>
   );
 }
--- mycount/src/UseGithubUser.js
+++ mycount/src/UseGithubUser.js
@@ -3,13 +3,18 @@ import useSWR from "swr";
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export function UseGithubUser({ username }) {
-  const { data, error } = useSWR(
+  const { data, error, mutate } = useSWR(
     username ? `https://api.github.com/users/${username}` : null,
     fetcher
   );
 
+  function refresh() {
+    mutate();
+  }
+
   return {
     users: data,
     error,
+    refresh: refresh,
   };
 }
